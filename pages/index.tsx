2ea0ca60f834e00f5f1ecc8226e9e3e14764d55e
sanskar001 <sanskarm001@gmail.com>
2024-06-08 09:42:18 +0530

    feat: added movie list below billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <div>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </div>
   );
 }
